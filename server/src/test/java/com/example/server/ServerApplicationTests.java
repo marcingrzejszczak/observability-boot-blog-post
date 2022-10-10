0ab01085f35aaf672063cb84d10a558a2455b44c
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-10-10 13:39:54 +0200

    Added native section in the docs

--- server/src/test/java/com/example/server/ServerApplicationTests.java
+++ server/src/test/java/com/example/server/ServerApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.server;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureObservability
 class ServerApplicationTests {
 
 	@Test
