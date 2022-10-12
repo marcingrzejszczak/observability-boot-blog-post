4b9ffb3806a0c8e33b51d809ecffc9f4fc4e8fef
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-10-12 12:54:21 +0200

    Applied suggestions from Darek

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -1,5 +1,10 @@
 package com.example.client;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.observation.DefaultMeterObservationHandler;
 import io.micrometer.observation.Observation;
@@ -40,14 +45,16 @@ public class ClientApplication {
 	// tag::runner[]
 	@Bean
 	CommandLineRunner myCommandLineRunner(ObservationRegistry registry, RestTemplate restTemplate) {
+		Random highCardinalityValues = new Random(); // Simulates potentially large number of values
+		List<String> lowCardinalityValues = Arrays.asList("userType1", "userType2", "userType3"); // Simulates low number of values
 		return args -> {
 			// Example of using the Observability API manually
 			// <my.observation> is a "technical" name that does not depend on the context. It will be used to name e.g. Metrics
 			 Observation.createNotStarted("my.observation", registry)
 					 // Low cardinality means that the number of potential values won't be big. Low cardinality entries will end up in e.g. Metrics
-					.lowCardinalityKeyValue("low.cardinality.key", "low cardinality value")
+					.lowCardinalityKeyValue("userType", randomUserTypePicker(lowCardinalityValues))
 					 // High cardinality means that the number of potential values can be large. High cardinality entries will end up in e.g. Spans
-					.highCardinalityKeyValue("high.cardinality.key", "high cardinality value")
+					.highCardinalityKeyValue("userId", String.valueOf(highCardinalityValues.nextLong(100_000)))
 					 // <command-line-runner> is a "contextual" name that gives more details within the provided context. It will be used to name e.g. Spans
 					.contextualName("command-line-runner")
 					 // The following lambda will be executed with an observation scope (e.g. all the MDC entries will be populated with tracing information). Also the observation will be started, stopped and if an error occurred it will be recorded on the observation
@@ -60,4 +67,10 @@ public class ClientApplication {
 		};
 	}
 	// end::runner[]
+
+	Random randomUserTypePicker = new Random();
+
+	private String randomUserTypePicker(List<String> lowNumberOfValues) {
+		return lowNumberOfValues.get(randomUserTypePicker.nextInt(2));
+	}
 }
--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -2,16 +2,14 @@ package com.example.server;
 
 import java.util.Collections;
 import java.util.Random;
+import java.util.stream.StreamSupport;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.observation.DefaultMeterObservationHandler;
+import io.micrometer.common.KeyValue;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationHandler;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.annotation.Observed;
 import io.micrometer.observation.aop.ObservedAspect;
-import io.micrometer.tracing.Tracer;
-import io.micrometer.tracing.handler.TracingAwareMeterObservationHandler;
 import jakarta.servlet.DispatcherType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +23,7 @@ import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.observation.HttpRequestsObservationFilter;
 
@@ -65,31 +64,37 @@ public class ServerApplication {
 class MyController {
 
 	private static final Logger log = LoggerFactory.getLogger(MyController.class);
-	private final MyService myService;
+	private final MyUserService myUserService;
 
-	MyController(MyService myService) {
-		this.myService = myService;
+	MyController(MyUserService myUserService) {
+		this.myUserService = myUserService;
 	}
 
-	@GetMapping("/foo")
-	String myMapping() {
+	@GetMapping("/{userId}")
+	String userName(@PathVariable("userId") String userId) {
 		log.info("Got a request");
-		return myService.foo();
+		return myUserService.userName(userId);
 	}
 }
 // end::controller[]
 
 // tag::service[]
 @Service
-class MyService {
+class MyUserService {
+
+	private static final Logger log = LoggerFactory.getLogger(MyUserService.class);
 
 	private final Random random = new Random();
 
 	// Example of using an annotation to observe methods
-	@Observed(name = "foo.metric",
-			contextualName = "my-contextual-name",
-			lowCardinalityKeyValues = {"low.cardinality.key", "low cardinality value"})
-	String foo() {
+	// <user.name> will be used as a metric name
+	// <getting-user-name> will be used as a span  name
+	// <userType=userType2> will be set as a tag for both metric & span
+	@Observed(name = "user.name",
+			contextualName = "getting-user-name",
+			lowCardinalityKeyValues = {"userType", "userType2"})
+	String userName(String userId) {
+		log.info("Getting user name for user with id <{}>", userId);
 		try {
 			Thread.sleep(random.nextLong(200L)); // simulates latency
 		}
@@ -110,17 +115,25 @@ class MyHandler implements ObservationHandler<Observation.Context> {
 
 	@Override
 	public void onStart(Observation.Context context) {
-		log.info("Before running the observation for context [{}]", context.getName());
+		log.info("Before running the observation for context [{}], userType [{}]", context.getName(), getUserTypeFromContext(context));
 	}
 
 	@Override
 	public void onStop(Observation.Context context) {
-		log.info("After running the observation for context [{}]", context.getName());
+		log.info("After running the observation for context [{}], userType [{}]", context.getName(), getUserTypeFromContext(context));
 	}
 
 	@Override
 	public boolean supportsContext(Observation.Context context) {
 		return true;
+	}
+
+	private String getUserTypeFromContext(Observation.Context context) {
+		return StreamSupport.stream(context.getLowCardinalityKeyValues().spliterator(), false)
+				.filter(keyValue -> "userType".equals(keyValue.getKey()))
+				.map(KeyValue::getValue)
+				.findFirst()
+				.orElse("UNKNOWN");
 	}
 }
 // end::handler[]
